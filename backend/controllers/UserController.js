b5d420d1b6cf4f3bf1d5cd07786897205c83367d
joyce <joycemendes.jm79@gmail.com>
2022-12-15 18:25:16 -0300

    registro de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -15,7 +15,37 @@ const generateToken = (id) => {
 //REGISTER USER AND SIGN IN 
 const register = async(req, res) => {
 
-    res.send("Registro");
+   const {name, email, password} = req.body;
+
+   //CHECK IF USER EXISTS
+   const user = await User.findOne({email});
+
+   if(user){
+    res.status(422).json({errors: ["Por favor, utilize outro e-mail"]});
+    return
+   };
+
+   //Generate password hash
+   const salt = await bcrypt.genSalt();
+   const passwordHash = await bcrypt.hash(password, salt);
+
+   //CREATE USER
+   const newUser = await User.create({
+        name,
+        email, 
+        password: passwordHash
+   });
+
+   // CHECK IF USER WAS CREATED SUCCESFULLY
+   if(!newUser){
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde!"]});
+        return
+   }
+
+   res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+   });
 
 };
 
