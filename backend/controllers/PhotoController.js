aa4f7b0db16d4a8767d3ed8b4d9e1d61fe5a5e9d
joyce <joycemendes.jm79@gmail.com>
2022-12-20 14:06:20 -0300

    atualização de foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -100,10 +100,42 @@ const getPhotoById = async(req, res) => {
 
 };
 
+//UPDATE A PHOTO
+const updatePhoto =  async(req, res) => {
+
+    const {id} = req.params;
+    const {title} = req.body;
+
+    const reqUser = req.user;
+    const photo = await Photo.findById(id);
+
+    //CHECK IF PHOTO EXISTS
+    if(!photo){
+        res.status(404).json({errors: ["Foto não encontrada!"]});
+        return;
+    };
+
+    //CHECK IF PHOTO BELONGS TO USER
+    if(!photo.userId.equals(reqUser._id)){
+
+        res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde!"]});
+        return;
+    };
+
+    if(title){
+        photo.title = title;
+    };
+
+    await photo.save();
+    res.status(200).json({photo, message: "Foto atualizada com sucesso!"});
+
+};
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
     getPhotoById,
+    updatePhoto,
 };
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 //CONTROLLER
-const { insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById } = require("../controllers/PhotoController");
+const { insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto } = require("../controllers/PhotoController");
 
 //MIDDLEWARES
-const { photoInsertValidation } = require("../middlewares/photoValidation");
+const { photoInsertValidation, photoUpdateValidation } = require("../middlewares/photoValidation");
 const authGard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload } = require("../middlewares/imageUpload")
@@ -16,6 +16,7 @@ router.delete("/:id", authGard, deletePhoto);
 router.get("/", authGard, getAllPhotos);
 router.get("/user/:id", authGard, getUserPhotos);
 router.get("/:id", authGard, getPhotoById);
+router.put("/:id", authGard,photoUpdateValidation(), validate, updatePhoto);
 
 
 module.exports = router;
