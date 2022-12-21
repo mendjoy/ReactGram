0cf1aa08061c4f5544dfadbf5c0d63e74c2cf471
joyce <joycemendes.jm79@gmail.com>
2022-12-21 13:24:40 -0300

    states do form de cadastro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -9,9 +9,21 @@ import { useState, useEffect } from "react";
 
 const Register = () => {
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const user = {
+      name, 
+      email, 
+      password, 
+      confirmPassword
+    }
   };
 
   return (
@@ -20,10 +32,26 @@ const Register = () => {
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome"/>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha"/>
-        <input type="password" placeholder="Confirme a senha" />
+        <input type="text" 
+          placeholder="Nome" 
+          onChange={(e) => setName(e.target.value)} 
+          value={name || ""}/>
+
+        <input type="email" 
+          placeholder="E-mail"  
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email || ""}/>
+
+        <input type="password" 
+          placeholder="Senha" o
+          nChange={(e) => setPassword(e.target.value)} 
+          value={email || ""}/>
+
+        <input type="password" 
+          placeholder="Confirme a senha" 
+          onChange={(e) => setConfirmPassword(e.target.name)}  
+          value={confirmPassword || ""}/>
+
         <input type="submit" value="Cadastrar" />
       </form>
 
