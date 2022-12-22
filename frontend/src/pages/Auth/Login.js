8bc668f195e924cfa2737c7672b6a98a23f7af8d
joyce <joycemendes.jm79@gmail.com>
2022-12-22 09:16:23 -0300

    form de login

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 import "./Auth.css";
 
 const Login = () => {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
   return (
-    <div>Login</div>
+    <div id="login">
+        <h2>ReactGram</h2>
+        <p className="subtitle">Entre para ver o que os seus amigos estão compartilhando.</p>
+
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="E-mail"/>
+          <input type="password" placeholder="Senha"/>
+          <input type="submit" value="Entrar"/>
+        </form>
+        <p>Não possui uma conta? <Link to="/register">Clique aqui.</Link></p>
+
+    
+    </div>
   )
 }
 
