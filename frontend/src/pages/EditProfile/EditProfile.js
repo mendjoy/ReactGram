8524ffa765fffd6c82f16c54104a47ebcb035967
joyce <joycemendes.jm79@gmail.com>
2023-01-04 16:58:49 -0300

    css do edit profile

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,8 +1,37 @@
-import "EditProfile.css";
+import "./EditProfile.css"
 
 const EditProfile = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
   return (
-    <div>EditProfile</div>
+    <div id="edit-profile">
+        <h2>Edite seus dados</h2>
+        <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
+
+        {/*preview da imagem */}
+        <form onSubmit={handleSubmit}>
+            <input type="text"  placeholder="Nome"/>
+            <input type="email"placeholder="E-mail"  disabled/>
+            <label>
+                <span>Imagem do perfil:</span>
+                <input type="file" />
+            </label>
+
+            <label>
+                <span>Bio:</span>
+                <input type="text" placeholder="Descrição do perfil" />
+            </label>
+            <label>
+                <span>Quer alterar sua senha?</span>
+                <input type="password" placeholder="Digite sua nova senha" />
+            </label>
+            <input type="submit" value="Atualizar"/>
+        </form>
+
+
+    </div>
   )
 }
 
